3ffd265e401d68d3c16e266a06baee0b04239091
Dan Slack <dan@terrazzotechnologies.com>
2016-06-24 12:17:03 -0500

    Added analytics

--- app/config.js
+++ app/config.js
@@ -1,11 +1,13 @@
 var CONFIG = {
         convention: "Spiel 2015",
         geeklist: "https://boardgamegeek.com/geeklist/174654/spiel-2015-preview",
-        showReleased: false
+        showReleased: false,
+        ga: 'UA-66179629-1'
 };
 
 /*var CONFIG = {
         convention: "GenCon 2015",
         geeklist: "https://boardgamegeek.com/geeklist/184821",
-        showReleased: true
+        showReleased: true,
+        ga: 'UA-64457644-1'
 };*/
